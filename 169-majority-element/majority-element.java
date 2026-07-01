dcba667ee7e41ca495889af6415a7c04d496b874
Sharwari Bhosale <sharwaribhosale13@gmail.com>
2026-07-01 15:37:56 +0530

    Time: 2 ms (69.85%) | Memory: 55.5 MB (59.53%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+// Class containing the majority element logic
+class Solution {
+    // Function to find the majority element in an array
+    public int majorityElement(int[] nums) {
+        // Size of the given array
+        int n = nums.length;
+        
+        // Count variable
+        int cnt = 0;
+        
+        // Candidate element
+        int el = 0;
+        
+        // Step 1: Find the potential majority element
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0) {
+                cnt = 1;
+                el = nums[i];
+            } else if (el == nums[i]) {
+                cnt++;
+            } else {
+                cnt--;
+            }
+        }
+        
+        // Step 2: Verify the candidate
+        int cnt1 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el) {
+                cnt1++;
+            }
+        }
+        
+        // Return the element if it's a majority
+        if (cnt1 > (n / 2)) {
+            return el;
+        }
+        
+        // No majority found
+        return -1;
+    }
+}
+
