2ca0ac822599796a7fedc721cb2a53d2c8b0a5b8
Sharwari Bhosale <sharwaribhosale13@gmail.com>
2026-07-02 20:05:19 +0530

    Time: 2 ms (97.50%) | Memory: 45 MB (89.72%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,48 +1,21 @@
 import java.util.*;
-
 class Solution {
-    /* Function to find the longest substring
-       without repeating characters */
     public int lengthOfLongestSubstring(String s) {
         int n = s.length();
-
-        // Assuming all ASCII characters
         int HashLen = 256;
-
-        /* Hash table to store last
-           occurrence of each character */
         int[] hash = new int[HashLen];
-
-        /* Initialize hash table with
-           -1 (indicating no occurrence) */
         Arrays.fill(hash, -1);
-
         int l = 0, r = 0, maxLen = 0;
         while (r < n) {
-            /* If current character s.charAt(r) 
-               is already in the substring */
-            if (hash[s.charAt(r)] >= l) {
-                /* Move left pointer to the right
-                   of the last occurrence of s.charAt(r) */
+            if (hash[s.charAt(r)] >= l) { 
                 l = Math.max(hash[s.charAt(r)] + 1, l);
             }
-
-            // Calculate the current substring length
             int len = r - l + 1;
-
-            // Update maximum length found so far
             maxLen = Math.max(len, maxLen);
-
-            /* Store the index of the current
-               character in the hash table */
-            hash[s.charAt(r)] = r;
-
-            // Move right pointer to next position
+            hash[s.charAt(r)] = r;  
             r++;
         }
-
-        // Return the maximum length found
-        return maxLen;
+         return maxLen;
     }
 }
 
