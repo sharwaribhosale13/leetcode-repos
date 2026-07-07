4374119672fa2a13c9df99e712bcb6707c13f80f
Sharwari Bhosale <sharwaribhosale13@gmail.com>
2026-07-08 03:04:02 +0530

    Time: 38 ms (22.32%) | Memory: 59.1 MB (59.71%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -6,34 +6,33 @@ class Solution {
 
         for (int i = 0; i < nums.length - 2; i++) {
 
-            // Skip duplicate first elements
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
 
-            int left = i + 1;
-            int right = nums.length - 1;
+            int j = i + 1;
+            int k = nums.length - 1;
 
-            while (left < right) {
+            while (j < k) {
 
-                int sum = nums[i] + nums[left] + nums[right];
+                int sum = nums[i] + nums[j] + nums[k];
 
                 if (sum == 0) {
 
-                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
 
-                    left++;
-                    right--;
+                    j++;
+                    k--;
 
-                    while (left < right && nums[left] == nums[left - 1])
-                        left++;
+                    while (j < k && nums[j] == nums[j - 1])
+                        j++;
 
-                    while (left < right && nums[right] == nums[right + 1])
-                        right--;
+                    while (j < k && nums[k] == nums[k + 1])
+                        k--;
 
                 } else if (sum < 0) {
-                    left++;
+                    j++;
                 } else {
-                    right--;
+                    k--;
                 }
             }
         }
