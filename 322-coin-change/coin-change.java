3c14a2ea4e625dee0df8f2561710a8682db24ed5
Sharwari Bhosale <sharwaribhosale13@gmail.com>
2026-07-15 21:24:40 +0530

    Time: 16 ms (68.11%) | Memory: 46.3 MB (60.95%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,30 +1,23 @@
 import java.util.*;
 
-// Solution class to implement tabulation approach
 class Solution {
-    // Function to find minimum coins
     public int coinChange(int[] coins, int amount) {
-        // Creating dp array of size amount+1
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
 
-        // Base case: dp[0] = 0
+        int[] dp = new int[amount + 1];
+
+        Arrays.fill(dp, amount + 1); // A value greater than the maximum possible answer
         dp[0] = 0;
 
-        // Loop through all amounts from 1 to amount
         for (int i = 1; i <= amount; i++) {
-            // Try each coin
             for (int coin : coins) {
-                // If coin can be used
-                if (i - coin >= 0 && dp[i - coin] != Integer.MAX_VALUE) {
-                    // Update dp[i] with minimum coins
-                    dp[i] = Math.min(dp[i], 1 + dp[i - coin]);
+
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
                 }
+
             }
         }
 
-        // If dp[amount] is still infinity, return -1
-        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+        return dp[amount] > amount ? -1 : dp[amount];
     }
 }
-
