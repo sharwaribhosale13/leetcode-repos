270aa7a3f11884d7150dfaaa996f37ed1ec23c4c
Sharwari Bhosale <sharwaribhosale13@gmail.com>
2026-07-16 22:39:01 +0530

    Time: 11 ms (95.92%) | Memory: 47.5 MB (81.93%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -4,26 +4,43 @@ class Solution {
 
     public int[] topKFrequent(int[] nums, int k) {
 
+        // Count frequency of each number
         Map<Integer, Integer> map = new HashMap<>();
 
         for (int num : nums)
             map.put(num, map.getOrDefault(num, 0) + 1);
 
-        PriorityQueue<Integer> pq =
-                new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
+        // Bucket array: index = frequency
+        List<Integer>[] bucket = new ArrayList[nums.length + 1];
 
+        // Put numbers into their frequency bucket
         for (int num : map.keySet()) {
 
-            pq.offer(num);
+            int freq = map.get(num);
 
-            if (pq.size() > k)
-                pq.poll();
+            if (bucket[freq] == null)
+                bucket[freq] = new ArrayList<>();
+
+            bucket[freq].add(num);
         }
 
         int[] ans = new int[k];
+        int index = 0;
 
-        for (int i = k - 1; i >= 0; i--)
-            ans[i] = pq.poll();
+        // Traverse buckets from highest frequency
+        for (int i = bucket.length - 1; i >= 0 && index < k; i--) {
+
+            if (bucket[i] != null) {
+
+                for (int num : bucket[i]) {
+
+                    ans[index++] = num;
+
+                    if (index == k)
+                        break;
+                }
+            }
+        }
 
         return ans;
     }
