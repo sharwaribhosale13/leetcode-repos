ab8f5159344f3c40633907ee8ac0f1f1d024ff56
Sharwari Bhosale <sharwaribhosale13@gmail.com>
2026-08-04 20:46:51 +0530

    Time: 1 ms (97.37%) | Memory: 47.8 MB (26.04%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -62,18 +62,5 @@ class Solution {
     }
 }
 
-// Main class to run the program
-public class Main {
-    public static void main(String[] args) {
-        Solution obj = new Solution();
-        int[][] matrix = {{0,1,2,0},{3,4,5,2},{1,3,1,5}};
-        obj.setZeroes(matrix);
-        for (int[] row : matrix) {
-            for (int val : row) {
-                System.out.print(val + " ");
-            }
-            System.out.println();
-        }
-    }
-}
+
 
